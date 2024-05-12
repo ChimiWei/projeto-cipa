73076782420ef86a9facd049aac9ad8a09224c2a
Wei Oliveira <87078907+ChimiWei@users.noreply.github.com>
2024-05-12 20:09:12 -0300

    feat: Showing image from mssql db part III

--- public/js/votacao.js
+++ public/js/votacao.js
@@ -1,15 +1,17 @@
 const modal =  document.getElementById('candidatoModal')
 
-const candidatoModal = (CHAPA, NOME, SECAO, N_VOTACAO) => {
+const candidatoModal = (CHAPA, NOME, SECAO, N_VOTACAO, IMAGEM) => {
    modal.classList.toggle('show')
    let funcEl = document.getElementById('Func')
    let secaoEl = document.getElementById('Secao')
    let NVotacaoEl = document.getElementById('NVotacao')
+   let imgEl = document.getElementById('Imagem')
    let btnConfirmarVotoEl = document.getElementById('btnConfirmarVoto')
    console.log(CHAPA)
    funcEl.textContent = `Funcionário: ${CHAPA} ${NOME}`
    secaoEl.textContent = `Seção: ${SECAO}`
    NVotacaoEl.textContent = N_VOTACAO
+   imgEl.src = IMAGEM
    btnConfirmarVotoEl.value = N_VOTACAO
     
 }
