ff5dfc484075ac5e48c1aebcd688a741af9d0b16
Wei Oliveira <87078907+ChimiWei@users.noreply.github.com>
2024-05-14 17:52:51 -0300

    fix: omg i'll smt myself

--- src/helpers/isTodayInRange.js
+++ src/helpers/isTodayInRange.js
@@ -1,26 +1,28 @@
 
 function isTodayInRange(firstDate, lastDate) {
-    const today = new Date().toLocaleDateString('pt-br', {
+    const today = new Date().toLocaleDateString('en-US', {
         day: '2-digit',
         month: '2-digit',
         year: 'numeric',
     })
 
+    console.log(`TESTE: ${today} está entre ${firstDate} e ${lastDate}`)
 
-
-    const first = new Date(Date.UTC(firstDate.getUTCFullYear(), firstDate.getUTCMonth(), firstDate.getUTCDate())).toLocaleDateString('pt-br', {
+    const first = new Date(Date.UTC(firstDate.getUTCFullYear(), firstDate.getUTCMonth(), firstDate.getUTCDate())).toLocaleDateString('en-US', {
         day: '2-digit',
         month: '2-digit',
         year: 'numeric',
     })
 
-    const last = new Date((Date.UTC(lastDate.getUTCFullYear(), lastDate.getUTCMonth(), lastDate.getUTCDate() + 1))).toLocaleDateString('pt-br', {
+    const last = new Date((Date.UTC(lastDate.getUTCFullYear(), lastDate.getUTCMonth(), lastDate.getUTCDate() + 20))).toLocaleDateString('en-US', {
         day: '2-digit',
         month: '2-digit',
         year: 'numeric',
     })
 
     console.log(`${today} está entre ${first} e ${last}`)
+
+    console.log(today >= last)
 
     return (first <= today && today <= last)
 }
