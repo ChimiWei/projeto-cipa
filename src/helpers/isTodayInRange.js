41d9ea691fae0b753455d78e699dec5fcf86378c
Wei Oliveira <87078907+ChimiWei@users.noreply.github.com>
2024-05-14 09:00:02 -0300

    fix: date fix (AGAIN)

--- src/helpers/isTodayInRange.js
+++ src/helpers/isTodayInRange.js
@@ -1,6 +1,6 @@
 
 function isTodayInRange(firstDate, lastDate) {
-    const today = new Date().toLocaleDateString('en-US', {
+    const today = new Date().toLocaleDateString('pt-br', {
         day: '2-digit',
         month: '2-digit',
         year: 'numeric',
@@ -8,17 +8,19 @@ function isTodayInRange(firstDate, lastDate) {
 
 
 
-    const first = new Date(Date.UTC(firstDate.getUTCFullYear(), firstDate.getUTCMonth(), firstDate.getUTCDate())).toLocaleDateString('en-US', {
+    const first = new Date(Date.UTC(firstDate.getUTCFullYear(), firstDate.getUTCMonth(), firstDate.getUTCDate())).toLocaleDateString('pt-br', {
         day: '2-digit',
         month: '2-digit',
         year: 'numeric',
     })
 
-    const last = new Date(Date.UTC(lastDate.getUTCFullYear(), lastDate.getUTCMonth(), lastDate.getUTCDate())).toLocaleDateString('en-US', {
+    const last = new Date((Date.UTC(lastDate.getUTCFullYear(), lastDate.getUTCMonth(), lastDate.getUTCDate() + 1))).toLocaleDateString('pt-br', {
         day: '2-digit',
         month: '2-digit',
         year: 'numeric',
     })
+
+    console.log(`${today} está entre ${first} e ${last}`)
 
     return (first <= today && today <= last)
 }
