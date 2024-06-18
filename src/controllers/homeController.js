560b01514f3d1e7f1295b124742e9a47d327e8f1
Wei Oliveira <87078907+ChimiWei@users.noreply.github.com>
2024-06-18 17:41:37 -0300

    Revert "feat: new home page"
    This reverts commit 13a13936d1dc6ce4e85fd93168346853b674b047.

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,17 +1,10 @@
-const mailRepo = require("../helpers/mail-repo")
-
 
 const homeController = {
-    renderHome: (req, res) => {
+     renderHome: 
+        (req, res) => {
             
             res.render('home.ejs', {})
-    },
-
-    postMail: async (req, res) => {
-        await mailRepo.budget(req.body.email)
-
-        res.redirect('/')
-    }
+        }
 }
 
 
