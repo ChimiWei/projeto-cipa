cd3cfee5d70de2927fdbe680e7bc834f348af667
Wei Oliveira <87078907+ChimiWei@users.noreply.github.com>
2024-06-24 09:27:09 -0300

    fix: need the port option on mysql

--- config/db_connection_mysql.js
+++ config/db_connection_mysql.js
@@ -5,7 +5,8 @@ const pool = createPool({
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    connectionLimit: 10
+    port: process.env.MYSQL_PORT,
+    connectionLimit: 5
 });
 
 
