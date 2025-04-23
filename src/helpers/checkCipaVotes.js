a8efebdc7abe0961a1c4b68204a30d84c2c7e5dd
Wei Oliveira <87078907+ChimiWei@users.noreply.github.com>
2025-04-23 09:02:04 -0300

    fix: check vote remove

--- src/helpers/checkCipaVotes.js
+++ src/helpers/checkCipaVotes.js
@@ -6,18 +6,20 @@ const checkCipaVotes = async (codfilial, cipaid) => {
     const [rows] = await mysqlPromise.query(...repository.mysql.getTotalVotos(cipaid))
     console.log(rows)
 
-    const [filial] = result
-    const [votos] = rows
+    // const [filial] = 1
+    // const [votos] = rows
 
-    let percentage = Math.floor((votos.total * 100) / filial.total)
+    // let percentage = Math.floor((votos.total * 100) / filial.total)
 
-    console.log(percentage + '%')
+    // console.log(percentage + '%')
 
-    if (percentage > 50) {
-        return true
-    } else {
-        return false
-    }
+    // if (percentage > 50) {
+    //     return true
+    // } else {
+    //     return false
+    // }
+
+    return false
 
 }
 
